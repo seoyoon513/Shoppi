c8024b5e5234bc9fc121436b9857bdfdb2c3e0d0
seoyoon513 <syoonlee513@gmail.com>
2022-08-11 16:31:17 +0900

    add SplashActivity

---
+++ app/src/main/java/com/syoon/shoppi/SplashActivity.kt
@@ -0,0 +1,15 @@
+package com.syoon.shoppi
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+//API 30 이하 버전 Splash screen
+class SplashActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
